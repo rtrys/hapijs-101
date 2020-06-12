cb6a04f3742afffc379676faaf412e406278fb43
Hugo Ortiz <huortiz@paypal.com>
2020-06-11 18:11:14 -0600

    [01-create-a-route] routes created

--- README.md
+++ README.md
@@ -2,9 +2,20 @@
 
 Example project to build a simple HapiJS App, you can see the whole example at master branch or see the steps in branches
 
+## usage
+- run `npm install`
+- run `npm run start:dev`
+- enjoy
+
 ## Branches
 
 ### feature/01-create-your-server
 - Hapi and nodemon are installed
 - Server is created at `index.js` file
+
+### feature/01-create-a-route
+- Routes are created
+  - `/`
+  - `/hello/:name`
+  - `/json`
 
--- index.js
+++ index.js
@@ -9,6 +9,32 @@ const init = async () => {
     host: 'localhost'
   });
 
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => 'Hello World!',
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/hello/{name}',
+    handler: (request, h) => {
+      return `Hello ${request.params.name}`
+    }
+  });
+
+  server.route({  
+    method: 'GET',
+    path: '/json',
+    handler: (request, h) => {
+      return {
+        key: 'value',
+        another: false,
+        number: 10,
+      };
+    },
+  });
+
   await server.start();
   console.log('Server running on %s', server.info.uri);
 };
